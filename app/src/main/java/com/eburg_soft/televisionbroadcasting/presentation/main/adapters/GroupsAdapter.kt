14505e6499a13813d22263e631797e391c7a5f16
Stas Anishchenko <anischenko_stas@mail.ru>
2020-09-30 16:45:06 +0500

    - created GroupsAdapter.kt, GroupsDiffCallback.kt;
    - changed TVMenuFragment.kt, TVMenuPresenter.kt

--- app/src/main/java/com/eburg_soft/televisionbroadcasting/presentation/main/adapters/GroupsAdapter.kt
+++ app/src/main/java/com/eburg_soft/televisionbroadcasting/presentation/main/adapters/GroupsAdapter.kt
@@ -1,2 +1,22 @@
 package com.eburg_soft.televisionbroadcasting.presentation.main.adapters
 
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import com.eburg_soft.televisionbroadcasting.core.BaseAdapter
+import com.eburg_soft.televisionbroadcasting.data.datasource.database.models.GroupEntity
+
+class GroupAdapter :BaseAdapter<GroupEntity,GroupAdapter>(GroupDiffCallback()){
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseViewHolder {
+        return
+    }
+
+    class GroupViewHolder(val view:View):BaseViewHolder(view){
+
+        override fun onBind(item: Any) {
+            TODO("Not yet implemented")
+        }
+    }
+}
+
