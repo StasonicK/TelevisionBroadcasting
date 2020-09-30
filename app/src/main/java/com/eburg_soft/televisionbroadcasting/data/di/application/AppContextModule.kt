6f1f8744467e6b237aaedf61b8bf524a1fd9b6ef
Stas Anishchenko <anischenko_stas@mail.ru>
2020-10-01 00:51:54 +0500

    - di finished

--- app/src/main/java/com/eburg_soft/televisionbroadcasting/data/di/application/AppContextModule.kt
+++ app/src/main/java/com/eburg_soft/televisionbroadcasting/data/di/application/AppContextModule.kt
@@ -2,18 +2,16 @@ package com.eburg_soft.televisionbroadcasting.data.di.application
 
 import android.app.Application
 import android.content.Context
+import com.eburg_soft.televisionbroadcasting.data.di.base.AppContext
 import dagger.Module
 import dagger.Provides
 import javax.inject.Singleton
 
 @Module
-class AppModule(private val application: Application) {
+class AppModule(private val context: Context) {
 
     @Singleton
     @Provides
-    fun provideApplication(): Application = application
-
-    @Singleton
-    @Provides
-    fun provideContext(): Context = application.applicationContext
+//    @AppContext
+    fun provideContext(): Context = context
 }
